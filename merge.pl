8f50c0355aec655652c2104c73c69f8bf9272216
fcastroginer <fcastroginer@gmail.com>
2014-03-20 19:02:34 +0000

    Use Header Names for merging

--- merge.pl
+++ merge.pl
@@ -9,12 +9,17 @@ my $res = GetOptions ("1:s"  => \$opts{1}, "2:s"  => \$opts{2}, "h:s" => \$opts{
 
 
 die(qq/
-Description: merge two files using selected columns. The output will contain file2 first. By default output will contain only the interesection. If there are duplicated values in file1, only the first value will be merged. It accepts 'gz' files
+Description: merge two files using selected columns. 
+	- Columns can be specified as column number or column name. In that case, header must be present in both files. If column names are different, column name from file2 will be used.
+	- The output will contain file2 columns first. 
+	- By default output will contain only the interesection. 
+	- If there are duplicated values in file1, only the first value will be merged. 
+	- It accepts 'gz' files
 
-Usage:   merge.pl [-h -d -f2 -a -empty NA] -1 col1 -2 col2  <file1> <file2>
+Usage:   merge.pl [-h -d -f2 -a -empty NA] -1 col1\/name1 -2 col2\/name  <file1> <file2>
 
 Options:
-	-h	BOOL	Join headers. Require header in both files. Unset by default
+	-h	BOOL	Join headers. Require header in both files. Unset by default. Activated if column names are used
 	-d	CHAR	Column separator. Default "\\t"
 	-a	BOOL	Print all lines from file2. Unset by default
 	-empty	CHAR	Character for emtpy fields. Requires option -a . None by default
@@ -24,9 +29,25 @@ Contact: fcastroginer\@gmail.com
 \n/) if (@ARGV != 2 || ! defined $opts{1} || ! defined $opts{2});	
 
 
+# Check column class
+my $coltype;
+if ($opts{1} =~ /\d+/ && $opts{2} =~ /\d+/) {
+	$coltype = "numeric";
+} elsif ($opts{1} =~ /\D+/ && $opts{2} =~ /\D+/) {
+	$coltype = "string";
+} else {
+	die ("ERROR: specified columns must have the same format : numeric or string");
+}
+$opts{h} = 1 if ($coltype eq "string");
+
+# Column Variable
+my $col1 = $opts{1};
+my $col2 = $opts{2};
+
 # Convert column number 1-based to 0-based
-my $col1 = $opts{1} - 1;
-my $col2 = $opts{2} - 1;
+$col1 = $opts{1} - 1 if($coltype eq "numeric");
+$col2 = $opts{2} - 1 if($coltype eq "numeric");
+
 
 # Reading file one
 if ($ARGV[0] =~ m/\.gz$/){
@@ -45,13 +66,23 @@ if (defined($opts{h}))
 	my $l = <FILE1>;
 	chomp $l;
 	my @l = split $opts{d},$l;
+	
+	# Get column index if coltype = string
+	if($coltype eq "string")
+	{
+		my @tmp = grep { $l[$_] eq $col1 } 0..$#l;
+		$col1 = $tmp[0];
+	}
+
 	my @o;
-        foreach my $index (0 .. $#l){
-                push(@o, $l[$index]) if($index != $col1);
-        }
+	foreach my $index (0 .. $#l){
+		push(@o, $l[$index]) if($index != $col1);
+	}
 	$header1 = join($opts{d}, @o);
 }
 
+
+ 
 # Reading rest of the file
 while(<FILE1>)
 {
@@ -88,6 +119,15 @@ if (defined($opts{h}))
 	chomp $header2;
 	print $header2.$opts{d}.$header1."\n" if (!defined $opts{f2});
 	print $header2."\n" if (defined $opts{f2});
+	
+	# Get column index if coltype = string
+	if($coltype eq "string")
+	{
+		my @l = split $opts{d},$header2;
+		my @tmp = grep { $l[$_] eq $col2 } 0..$#l;
+		$col2 = $tmp[0];
+	}
+	
 }
 
 
